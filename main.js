3233b1de393f1ad0e104bf89932d734403004385
suryansh2013 <124265177+suryansh2013@users.noreply.github.com>
2023-07-20 15:03:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Webcam.set({
+    width: 300,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log("ml5 Version: ", ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/pmJSFM1uU/model.json',modelLoaded);
+function  modelLoaded() {
+    console.log("Model Loaded !!!");
+}
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("name").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
